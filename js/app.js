d9871223d3db292332e995ec5c643dffdf360c39
cepuii <54589413+cepuii@users.noreply.github.com>
2023-10-22 10:38:12 +0300

    connect bootstrap

--- js/app.js
+++ js/app.js
@@ -24,26 +24,28 @@ const deleteTodo = event => {
 }
 
 const addTodoOnPage = formData => {
-    const todosList = document.querySelector('#todos');
+    const todosList = document.querySelector('.todos');
 
     const div = document.createElement('div');
     div.classList.add('todo-container');
 
     const h3 = document.createElement('h3');
     h3.innerText = formData.title;
+    h3.classList.add('todoItem');
     div.appendChild(h3);
 
     const text = document.createElement('p');
+    text.classList.add('todoItem');
     text.innerText = formData.taskText;
     div.appendChild(text);
 
     const deleteButton = document.createElement('button');
-    deleteButton.classList.add('deleteButton');
+    deleteButton.classList.add('btn', 'btn-light', 'deleteButton');
     deleteButton.addEventListener('click', () => {
         deleteTodo(event);
         location.reload();
     });
-    deleteButton.innerText = 'Delete TODO';
+    deleteButton.innerText = 'Delete task';
     div.appendChild(deleteButton)
 
     todosList.appendChild(div);
@@ -54,18 +56,13 @@ window.addEventListener('DOMContentLoaded', () =>{
     getTodos().forEach(element => addTodoOnPage(element));
 })
 
+   
 const form = document.querySelector('.addTodo');
-const inputs = form.querySelectorAll('input');
-const formData = {}
-for(let input of inputs){
-    input.addEventListener('change', function(event){
-        const {name, value} = event.target;
-        console.log(name, " ", value)
-        formData[name] = value;
-    })
-}
-
 form.addEventListener('submit', ()=>{
+    const formData = {}
+    const {title, taskText} = event.target.children;
+    formData.title = title.value;
+    formData.taskText = taskText.value;
     saveTodos(formData);
     addTodoOnPage(formData);
 });
